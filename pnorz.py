8f5981424efc22e69f31c6041f39f8ef47aec467
Vjb04 <42862210+Vjb04@users.noreply.github.com>
2018-08-31 14:29:07 +0530

    Create pnorz.py

---
+++ pnorz.py
@@ -0,0 +1,10 @@
+def main():
+    a=int(raw_input("Enter a number"))
+    if(a>0):
+      print("The number is positive")
+    elif(a<0):
+        print("The number is negative")
+    else:
+        print("The number is zero")
+        
+main()
