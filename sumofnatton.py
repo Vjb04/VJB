38b674f542fd8a932027ec2078be37bf00e061c4
Vjb04 <42862210+Vjb04@users.noreply.github.com>
2019-02-07 11:59:55 +0530

    Update sumofnatton.py

--- sumofnatton.py
+++ sumofnatton.py
@@ -1,9 +1,5 @@
-n=int(input('Enter a number: \n'))
-if(n<0):
-	print('Please Enter a positive number')
-else:
-	sum=0
-	while(n>0):
-		sum+=n
-		n-=1
-	print(sum)
+n = int(input())
+a=0
+for i in range(1,n+1):
+	a = a+i	
+print(a)
