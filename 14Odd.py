432b8d6692db5dc876834785721cc10f4b208d50
Vjb04 <42862210+Vjb04@users.noreply.github.com>
2019-02-08 13:33:10 +0530

    Update 14Odd.py

--- 14Odd.py
+++ 14Odd.py
@@ -1,7 +1,11 @@
-#Odd
+#ODD
 a=input().split()
 n=int(a[0])
 q=int(a[1])
+list=[]
 for i in range(n+1,q):
     if(i%2!=0):
-        print (i,end=' ')
+    	list.append(i)
+print(" ".join(map(str,list)))
+
+
