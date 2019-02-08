05567c896de38dd118660d995060d71cd88cc143
Vjb04 <42862210+Vjb04@users.noreply.github.com>
2019-02-08 13:33:57 +0530

    Update 15Even.py

--- 15Even.py
+++ 15Even.py
@@ -1,7 +1,10 @@
-#Even
+#EVEN
+
 a=input().split()
 n=int(a[0])
 q=int(a[1])
+list=[]
 for i in range(n+1,q):
     if(i%2==0):
-        print (i,end=' ')
+    	list.append(i)
+print(" ".join(map(str,list)))
