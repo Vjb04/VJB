9bd8d929b52a0765e9e638cb6a850ed4f5d67418
Vjb04 <42862210+Vjb04@users.noreply.github.com>
2019-02-20 14:29:21 +0530

    Create 21ap.py

---
+++ 21ap.py
@@ -0,0 +1,14 @@
+#ArithmaticProgression
+v=input().split()
+n=int(v[0])
+a=int(v[1])
+d=int(v[2])
+def sumofAP( a, d,n) : 
+    sum = 0
+    i = 0
+    while i < n : 
+        sum = sum + a 
+        a = a + d 
+        i = i + 1
+    return sum
+print (sumofAP(a, d, n))
